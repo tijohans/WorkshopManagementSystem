64bded4b5431b494b9945985e035256131dffe66
tijohans <tijohans@stud.ntnu.no>
2023-02-22 20:30:55 +0100

    Made overlay toggle button work

--- app/src/components/Header.tsx
+++ app/src/components/Header.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import Overlay from './Overlay'
 
 export default function Header() {
     const [isOpen, setOpen] = useState<Boolean>(false)
@@ -9,9 +10,12 @@ export default function Header() {
     }
 
     return (
-        <header className='h-20 flex justify-between items-center mb-5 px-[10vw] border-b-2 border-b-black'>
-            <img src="/wms_logo.svg" alt="Logo for the workshop management system" className='w-28' />
-            <button onClick={toggleOverlay}><img src="/icons/menu.svg" alt="" className='h-8' /></button>
-        </header>
+        <>
+            {isOpen && <Overlay />}
+            <header className='h-20 flex justify-between items-center mb-5 px-[10vw] border-b-2 border-b-black'>
+                <img src="/wms_logo.svg" alt="Logo for the workshop management system" className='w-28' />
+                <button onClick={toggleOverlay}><img src="/icons/menu.svg" alt="" className='h-8' /></button>
+            </header>
+        </>
     )
 }
---
+++ app/src/components/Overlay.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Overlay() {
+  return (
+    <nav className='h-full w-full absolute bg-gold'>
+        <p>I'm an overlay</p>
+    </nav>
+  )
+}
