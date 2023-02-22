7bfb374bb757aa1ed91891591686b51cf1c8e407
Haavaala <haavaala@stud.ntnu.no>
2023-02-22 20:37:00 +0100

    added a new component for the landing page

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,15 +1,18 @@
 import { useState } from 'react'
 import Button from './components/Button'
+import Landing from './components/Landing-Page'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold italic underline">
+      {/* <h1 className="text-3xl font-bold italic underline">
         Hello world!
-      </h1>
-      <Button text="kefaen"/>
+      </h1> */}
+      {/* <Button text="kefaen"/> */}
+      
+      {<Landing heading="Workshop Management System" paragraph="The place where you can view, find, and book all the tools you need. All from the same web-page"/>}
     </div>
   )
 }
