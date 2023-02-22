2ac7135b9423bf7cea63ea53605b3f04404af3cf
Haavaala <haavaala@stud.ntnu.no>
2023-02-22 21:26:58 +0100

    added styling to the component

--- app/src/components/Landing-Page.tsx
+++ app/src/components/Landing-Page.tsx
@@ -7,10 +7,12 @@ type LandingProps = {
 
 export default function Landing({heading, paragraph}: LandingProps) {
   return (
-    <div className="flex">
-    <h1 className="text-5xl font-bold">{heading}</h1>
-    <img src="../../public/WMS_image_logo.svg" alt="logo" className="object-right-top" />
-    <p>{paragraph}</p>
+    <div className="grid grid-cols-2 gap-30 m-0">
+      {/* <div> */}
+    <h1 className="text-4xl font-bold m-5">{heading}</h1>
+    <img src="../../public/WMS_image_logo.svg" alt="logo" className="col-span-1 justify-self-end" />
+    {/* </div> */}
+    <p className="text-lg w-80 font-bold m-5">{paragraph}</p>
     </div>
   )
 }
