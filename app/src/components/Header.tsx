d97d7cc9fdaf9a960e57b6ad58a283818dfc83af
tijohans <tijohans@stud.ntnu.no>
2023-02-23 21:24:24 +0100

    Deleted all the old code, and started over again. Made the navigation responsive, but took a whole different appraoch. Now it is working

--- app/src/components/Header.tsx
+++ app/src/components/Header.tsx
@@ -1,22 +1,38 @@
 import React, { useState } from 'react'
-import Overlay from './Overlay'
-import Navigation from './Navigation'
 
 export default function Header() {
-    const [isOpen, setOpen] = useState<Boolean>(false)
+    const [nav, setNav] = useState(false)
 
-    const toggleOverlay = () => {
-        setOpen(!isOpen)
-        console.log(isOpen)
+    const toggleNav = () => {
+        setNav(!nav)
     }
+
 
     return (
         <>
-            <header className={'h-20 flex justify-between items-center mb-5 px-[10vw] border-b-2 border-b-black'}>
+            <header className={'h-20 flex justify-between px-[5vw] items-center mb-5 border-b-2 border-b-black'}>
                 <img src="/wms_logo.svg" alt="Logo for the workshop management system" className='w-28' />
-                <button onClick={toggleOverlay}><img src="/icons/menu.svg" alt="" className='h-8 md:hidden' /></button>
+
+                <nav className='hidden md:flex'>
+                    <ul className='flex justify-center flex-row [&>*]:px-4'>
+                        <li className='hover:underline'>Home</li>
+                        <li className='hover:underline'>About</li>
+                        <li className='hover:underline'>Tools</li>
+                    </ul>
+                </nav>
+                
+                <button 
+                    className='md:hidden'
+                    onClick={toggleNav}>{nav ? <img src="/icons/menu.svg" alt="" /> : <img src="/icons/x.svg" alt="" />}</button>
+
+                <nav className={!nav ? 'md:hidden fixed top-20 left-0 w-full h-full bg-ghost-white ease-in-out duration-500' : 'fixed left-[-100%] top-20 md:hidden' }>
+                    <ul className='uppercase text-4xl font-bold [&>*]:m-5 [&>*]:my-10'>
+                        <li>Home</li>
+                        <li>About</li>
+                        <li>Tools</li>
+                    </ul>
+                </nav>
             </header>
-            {isOpen && <Overlay><Navigation /></Overlay>}
         </>
     )
 }
--- app/src/components/Overlay.tsx
+++
@@ -1,9 +0,0 @@
-import React, { useState} from 'react'
-
-export default function Overlay({children}: any) {
-    return (
-        <div className={'h-[60vh] w-full absolute bg-ghost-white animate-[slideIn_0.5s_ease-in-out]'}>
-            {children}
-        </div>
-    )
-}
