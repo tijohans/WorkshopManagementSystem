bd5b325361c1d5b7ab889bb8cdb05e49bf197ff4
Haavaala <haavaala@stud.ntnu.no>
2023-02-24 10:52:55 +0100

    added new logo and made a bit more responsive

--- app/src/components/Landing-Page.tsx
+++ app/src/components/Landing-Page.tsx
@@ -8,11 +8,9 @@ type LandingProps = {
 export default function Landing({heading, paragraph}: LandingProps) {
   return (
     <div className="grid grid-cols-2 gap-30 m-0">
-      {/* <div> */}
     <h1 className="text-4xl font-bold m-5">{heading}</h1>
-    <img src="../../public/WMS_image_logo.svg" alt="logo" className="col-span-1 justify-self-end" />
-    {/* </div> */}
-    <p className="text-lg w-80 font-bold m-5">{paragraph}</p>
+    <img src="../../public/full_wms_logo.svg" alt="logo" className="sm: col-span-1 ml-24 mt-20 relative " />
+    <p className="text-lg w-80 font-bold m-5 ">{paragraph}</p>
     </div>
   )
 }
