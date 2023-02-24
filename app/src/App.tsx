512c427615ee760e7b06c46ae3282eff6d311c8c
tijohans <tijohans@stud.ntnu.no>
2023-02-24 10:53:07 +0100

    Fixed open h1 tag

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -9,16 +9,10 @@ function App() {
   return (
     <div className="App bg-ghost-white">
       <Header />
-	  
-      <h1 className="text-3xl font-bold italic underline">
-	  
-      {/* <h1 className="text-3xl font-bold italic underline">
-        Hello world!
-      </h1> */}
-	  
-      {/* <Button text="kefaen"/> */}
-      
-      <Landing heading="Workshop Management System" paragraph="The place where you can view, find, and book all the tools you need. All from the same web-page"/>
+
+      <h1 className="text-3xl font-bold italic underline"></h1>
+
+      <Landing heading="Workshop Management System" paragraph="The place where you can view, find, and book all the tools you need. All from the same web-page" />
     </div>
   )
 }
