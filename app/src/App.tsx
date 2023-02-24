f96faebe6cdac0039a44870d2812740f90f2c36f
tijohans <tijohans@stud.ntnu.no>
2023-02-24 10:58:29 +0100

    Fixed bugs

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -11,7 +11,8 @@ function App() {
   return (
     <div className="App bg-ghost-white">
       <Header />
-      <Landing heading="Workshop Management System" paragraph="The place where you can view, find, and book all the tools you need. All from the same web-page"/>
+
+      <Landing heading="Workshop Management System" paragraph="The place where you can view, find, and book all the tools you need. All from the same web-page" />
     </div>
   )
 }
