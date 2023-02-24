1e1bb9c2f8003f48f780594a13466f168dbb20fb
Haavaala <haavaala@stud.ntnu.no>
2023-02-24 12:03:21 +0100

    thor fixed my problems on main :D

--- app/src/components/Landing-Page.tsx
+++ app/src/components/Landing-Page.tsx
@@ -9,7 +9,7 @@ export default function Landing({heading, paragraph}: LandingProps) {
   return (
     <div className="grid grid-cols-2 gap-30 m-0">
     <h1 className="text-4xl font-bold m-5">{heading}</h1>
-    <img src="/full_wms_logo.svg" alt="logo" className="relative object-contain col-span-1 ml-24 mt-20 sm:object-none sm:w-1/2" />
+    <img src="/full_wms_logo.svg" alt="logo" className="sm: relative col-span-1 ml-24 mt-20 " />
     <p className="text-lg w-80 font-bold m-5 ">{paragraph}</p>
     </div>
   )
