a18a2ac3ccd65dbc730e08477fbc08aec6db9e0f
tijohans <tijohans@stud.ntnu.no>
2023-02-24 10:53:07 +0100

    Fixed open h1 tag

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,8 +1,6 @@
 import { useState } from 'react'
 import Button from './components/Button'
-
 import Footer from './components/Footer'
-
 import Header from './components/Header'
 import Landing from './components/Landing-Page'
 
@@ -13,22 +11,7 @@ function App() {
   return (
     <div className="App bg-ghost-white">
       <Header />
-	  
-      <h1 className="text-3xl font-bold italic underline">
-	  
-      {/* <h1 className="text-3xl font-bold italic underline">
-        Hello world!
-
-      </h1>
-      <Button text="kefaen"/>
-      <Footer/>
-
-      </h1> */}
-	  
-      {/* <Button text="kefaen"/> */}
-      
       <Landing heading="Workshop Management System" paragraph="The place where you can view, find, and book all the tools you need. All from the same web-page"/>
-
     </div>
   )
 }
