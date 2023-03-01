4b359b2cb9498a0852234046d01e0e0935b6e06b
tijohans <tijohans@stud.ntnu.no>
2023-03-01 11:18:52 +0100

    Made list items in header responsive based on state input

--- app/src/components/Header.tsx
+++ app/src/components/Header.tsx
@@ -3,43 +3,46 @@ import React, { ReactNode, useState } from 'react'
 function listItems(): any {
     const pages: String[] = ['Home', 'About', 'Tools']
 
-    pages.map((page) => {
-        <li>{page}</li>
-    })
+    return (
+        // <ul className='flex justify-center flex-row [&>*]:px-4'>
+        pages.map(elem => {<li className='hover:underline'>{elem}</li>})
+        // </ul>
+    )
 }
 
 export default function Header() {
     const [nav, setNav] = useState(true)
+    const [pages, setPages] = useState(['home', 'about', 'tools'])
 
     const toggleNav = () => {
         setNav(!nav)
     }
 
+    // Generates all the list items
+    const generateListItems = () => {
+        return pages.map(elem => <li className='hover:underline'>{elem}</li>)
+    }
+
 
     return (
-        <>
-            <header className={'h-20 flex justify-between px-[5vw] items-center mb-5 border-b border-b-black'}>
-                <img src="/wms_logo.svg" alt="Logo for the workshop management system" className='w-28' />
+        <header className={'h-20 flex justify-between px-[5vw] items-center mb-5 border-b border-b-black'}>
+            <img src="/wms_logo.svg" alt="Logo for the workshop management system" className='w-28' />
 
-                <nav className='hidden md:flex'>
-                    <ul className='flex justify-center flex-row [&>*]:px-4'>
-                        <li className='hover:underline'>Home</li>
-                        <li className='hover:underline'>About</li>
-                        <li className='hover:underline'>Tools</li>
-                    </ul>
-                </nav>
-                
-                <button 
-                    className='md:hidden'
-                    onClick={toggleNav}>{nav ? <img src="/icons/menu.svg" alt="" /> : <img src="/icons/x.svg" alt="" />}</button>
+            <nav className='hidden md:flex'>
+                <ul className='flex justify-center flex-row [&>*]:px-4'>
+                    {generateListItems()}
+                </ul>
+            </nav>
+            
+            <button 
+                className='md:hidden'
+                onClick={toggleNav}>{nav ? <img src="/icons/menu.svg" alt="" /> : <img src="/icons/x.svg" alt="" />}</button>
 
-                <nav className={!nav ? 'md:hidden fixed top-20 left-0 w-full h-full bg-ghost-white ease-in-out duration-500' : 'fixed left-[-100%] top-20 md:hidden' }>
-                    <ul className='uppercase text-4xl font-bold [&>*]:m-5 [&>*]:my-10'>
-                        <li>test li</li>
-                        {listItems()}
-                    </ul>
-                </nav>
-            </header>
-        </>
+            <nav className={!nav ? 'md:hidden fixed top-20 left-0 w-full h-full bg-ghost-white ease-in-out duration-500' : 'fixed left-[-100%] top-20 md:hidden' }>
+                <ul className='uppercase text-4xl font-bold [&>*]:m-5 [&>*]:my-10'>
+                    {generateListItems()}
+                </ul>
+            </nav>
+        </header>
     )
 }
