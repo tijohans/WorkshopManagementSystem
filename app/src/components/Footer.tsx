3ef7e19a51075c6340f712dede7990473036aee1
lexavan <alexaev@stud.ntnu.no>
2023-03-01 12:24:17 +0100

    Changed border color

--- app/src/components/Footer.tsx
+++ app/src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <div className="fixed bottom-0 left-0 z-20 w-full p-4 bg-ghost-white border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6  md:w-screen ">
+    <div className=" fixed bottom-0 left-0 z-20 w-full p-4 bg-ghost-white border-t border-slate-300  shadow md:flex md:items-center md:justify-between md:p-6  md:w-screen ">
       <h2 className="text-3xl font-bold text-eerie-black">What</h2>
       <div>
       <p className="font-bold text-eerie-black">Contact us:</p>
