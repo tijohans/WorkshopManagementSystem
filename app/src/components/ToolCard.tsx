830c35151b84828ed05e848a15239d6f6f80e763
Haavaala <haavaala@stud.ntnu.no>
2023-03-01 11:32:44 +0100

    fixed responsiveness on toolcard

--- app/src/components/ToolCard.tsx
+++ app/src/components/ToolCard.tsx
@@ -9,9 +9,9 @@ type toolcardProps = {
 
 export default function ToolCard(props: toolcardProps) {
   return (
-    <div className='flex flex-wrap w-60 justify-center items-center h-72 border-2 md:h-52 md:w-80'>
+    <div className='flex flex-col justify-center items-center border-2 h-72 md:flex-row md:w-80 md:h-52'>
       <img className='w-52 h-32 ml-3' src={props.src} alt={props.alt} />
-      <div>
+      <div className='flex-row'>
       <h1 className='text-4xl font-bold ml-3'>{props.name}</h1>
       <p className='ml-3'>{props.paragraph}</p>
       </div>
