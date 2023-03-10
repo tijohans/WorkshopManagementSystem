017990df21e37a3d221f9311af7b29b571ea004e
lexavan <alexaev@stud.ntnu.no>
2023-03-10 15:07:17 +0100

    Small fixes

--- app/src/pages/UserPage.tsx
+++ app/src/pages/UserPage.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import Button from '../components/Button'
+import UserInfo from '../components/UserInfo'
 
 export default function UserPage() {
 
@@ -11,40 +12,7 @@ export default function UserPage() {
     <span className="font-medium text-ghost-white ">NN</span>
 </div>
 
-
-<form>
-<h3 className="text-2xl font-bold text-eerie-black">Navn Navnesen</h3>
-
-<div className="mb-6 w-11/12  ">
-    <label htmlFor="email" className="block mb-2 text-sm font-medium text-eerie-black ">Name</label>
-    <input type="email" id="disabled-input" aria-label="disabled input" className="bg-gray-50 border border-gray-300 text-eerie-black text-sm rounded-lg focus:ring-robin-egg-blue focus:border-robin-egg-blue block w-full p-2.5 cursor-not-allowed"   placeholder="Navn" required/>
-  </div>
- 
-  <div className="mb-6 w-11/12  ">
-    <label htmlFor="email" className="block mb-2 text-sm font-medium text-eerie-black ">Last name</label>
-    <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-eerie-black text-sm rounded-lg focus:ring-robin-egg-blue focus:border-robin-egg-blue block w-full p-2.5 cursor-not-allowed " placeholder="Navnesen" required/>
-  </div>
-
-  <div className="mb-6 w-11/12  ">
-    <label htmlFor="email" className="block mb-2 text-sm font-medium text-eerie-black ">Email</label>
-    <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-eerie-black text-sm rounded-lg focus:ring-robin-egg-blue focus:border-robin-egg-blue block w-full p-2.5 cursor-not-allowed " placeholder="navnnavnesen@stud.ntnu.no" required/>
-  </div>
-
-  <div className="mb-6 w-11/12  ">
-    <label htmlFor="email" className="block mb-2 text-sm font-medium text-eerie-black ">Password</label>
-    <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-eerie-black text-sm rounded-lg focus:ring-robin-egg-blue focus:border-robin-egg-blue block w-full p-2.5 cursor-not-allowed" placeholder="*******" required/>
-  </div>
-
-  <div className="mb-6 w-11/12  ">
-    <label htmlFor="email" className="block mb-2 text-sm font-medium text-eerie-black ">New password</label>
-    <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-eerie-black text-sm rounded-lg focus:ring-robin-egg-blue focus:border-robin-egg-blue block w-full p-2.5 cursor-not-allowed " placeholder="*******" required/>
-  </div>
-
-  
-  
- <Button text="Submit changes"/>
-</form>
-
+<UserInfo />
 
 
 
