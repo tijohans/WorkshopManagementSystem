822cad725fa8293603378952e23a13d14e48aff0
lexavan <alexaev@stud.ntnu.no>
2023-03-10 14:10:11 +0100

    Small fix

--- app/src/components/Header.tsx
+++ app/src/components/Header.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 export default function Header() {
     const [nav, setNav] = useState(true)
-    const [pages, setPages] = useState(['home', 'tools', 'Login', 'UserPage'])
+    const [pages, setPages] = useState(['home', 'tools', 'Login', ])
 
     const toggleNav = () => {
         setNav(!nav)
