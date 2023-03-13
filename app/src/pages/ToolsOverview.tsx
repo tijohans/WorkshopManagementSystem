ffd447a16f6fd06ab0d37e2865de46cd8db7424a
tijohans <tijohans@stud.ntnu.no>
2023-03-13 22:26:57 +0100

    Added loading animations when fetching data in toolcards and table, cleaned up components

--- app/src/pages/ToolsOverview.tsx
+++ app/src/pages/ToolsOverview.tsx
@@ -1,27 +1,12 @@
-import React, { useState, useEffect, useRef } from 'react'
-import ToolCard from '../components/ToolCard'
-import axios from 'axios'
-import Table from '../components/Table'
+import React, { useState } from 'react'
+import Table from '../components/ToolOverview/Table'
 import Button from '../components/Button'
+import ToolCards from '../components/ToolOverview/ToolCards'
+
 
 export default function ToolsOverview() {
 
-  const [tools, setTools] = useState<any[]>([])
   const [toggleView, setToggleView] = useState<boolean>(false)
-
-  useEffect(() => {
-    getTools()
-  }, []);
-
-  const getTools = () => {
-    axios.get(`https://wms-api-ps1s.onrender.com/api/tools`)
-      .then((response) => {
-        setTools(response.data)
-      })
-      .catch(error => console.error("Error: " + error))
-  }
-
-  const toolCards = tools.map((tool, key) => <ToolCard key={key} id={tool.id} src={tool.imageurl} name={tool.name} paragraph={tool.description} />)
 
   return (
     <div className="flex flex-wrap flex-col justify-center items-center gap-4 min-h-full">
@@ -29,11 +14,7 @@ export default function ToolsOverview() {
       {/* // ? Jallascript???? */}
       <span onClick={() => setToggleView(!toggleView)} ><Button text='toggle view' /></span>
 
-      {toggleView ? <Table name="tools" /> :
-        <div className='flex flex-wrap justify-center gap-6 mx-6'>
-          {toolCards}
-        </div>
-      }
+      {toggleView ? <Table name="tools" /> : <ToolCards />}
 
     </div>
   )
