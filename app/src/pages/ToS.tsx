3172a7725a27862b0af196d829b03f1a49ea50bb
tijohans <tijohans@stud.ntnu.no>
2023-03-15 13:05:59 +0100

    Added terms of service page and link in footer

---
+++ app/src/pages/ToS.tsx
@@ -0,0 +1,53 @@
+export default function ToS() {
+    return (
+        <section className="flex justify-evenly mx-10 my-5">
+            <article className="grid place-items-center min-h-[80vh]">
+                <h1 className="text-4xl mb-5 font-semibold">Terms of service</h1>
+                <ol className="max-w-[50ch]">
+                    <li className="mb-5">
+                        <h2 className="font-semibold">1. User Registration</h2>
+                        <p>By registering as a user on this website, you consent to providing us with your full name, email address, and password. We will store this information in order to provide you with services and improve your user experience.</p>
+                    </li>
+                    <li className="mb-5">
+                        <h2 className="font-semibold">2. Privacy</h2>
+                        <p>We take privacy seriously and will not share or sell your personal information with third parties. We will only use your personal information to deliver services to you and to improve our website.</p>
+                    </li>
+                    <li className="mb-5">
+                        <h2 className="font-semibold">3. Security</h2>
+                        <p>We take security seriously and will protect your personal information from unauthorized access, misuse, or loss. We use appropriate technical and organizational measures to protect your information.</p>
+                    </li>
+                    <li className="mb-5">
+                        <h2 className="font-semibold">4. Disclaimer of Liability</h2>
+                        <p>We will not be liable for any loss or damages resulting from unauthorized access to your account, or from you sharing your password with another person.</p>
+                    </li>
+                    <li className="mb-5">
+                        <h2 className="font-semibold">5. Updates</h2>
+                        <p>We reserve the right to change the terms of this agreement at any time. Any changes will be posted on our website. Continuing to use our website after changes have been posted means that you accept the new terms.</p>
+                    </li>
+                </ol>
+
+            </article>
+            <img src="/GDPR.svg" alt="" className="hidden lg:block md:max-w-xl" />
+        </section>
+    )
+}
+
+/* 
+    # Terms of Service
+
+    ## Brukerregistrering
+    Ved å registrere deg som bruker på denne nettsiden, samtykker du til å gi oss fullt navn, e-postadresse og passord. Vi vil lagre denne informasjonen for å kunne tilby deg tjenester og forbedre brukeropplevelsen din.
+
+    ## Personvern
+    Vi tar personvern på alvor, og vil ikke dele eller selge dine personlige opplysninger med tredjeparter. Vi vil kun bruke dine personlige opplysninger for å levere tjenester til deg og for å forbedre vår nettside.
+
+    ## Sikkerhet
+    Vi tar sikkerhet på alvor, og vil beskytte dine personlige opplysninger mot uautorisert tilgang, misbruk eller tap. Vi bruker passende tekniske og organisatoriske tiltak for å beskytte dine opplysninger.
+
+    ## Ansvarsfraskrivelse
+    Vi vil ikke være ansvarlig for eventuelle tap eller skader som følge av uautorisert tilgang til din konto, eller som følge av at du har delt ditt passord med en annen person.
+
+    ## Oppdateringer
+    Vi forbeholder oss retten til å endre vilkårene i denne avtalen når som helst. Eventuelle endringer vil bli publisert på vår nettside. Fortsettelse av bruk av vår nettside etter publisering av endringer betyr at du aksepterer de nye vilkårene.
+
+*/
