bbb0466f9580290adbc346380fcc99f94513755c
lexavan <alexaev@stud.ntnu.no>
2023-03-17 10:41:29 +0100

    small fixes

--- app/src/components/LoginForm.jsx
+++ app/src/components/LoginForm.jsx
@@ -1,26 +1,33 @@
 import React, { ReactNode, useState } from 'react'
 import Button from './Button'
 import SmallButton from './SmallButton'
+import { useForm } from 'react-hook-form';
+
 
 export default function LoginForm() {
 
-
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  
   
     return (
        
-<form className="top-1 h-200 flex flex-col justify-center items-center space-y-10 ">
-<h2 className="uppercase text-3xl font-bold text-eerie-black md:text-4xl ">User login</h2>
+<form className="top-1 h-200 flex flex-col justify-center items-center space-y-10" onSubmit={handleSubmit((data) => console.log(data))}>
+<h2 className="text-3xl font-bold text-eerie-black md:text-4xl ">User Login</h2>
   <div className="mb-6 w-11/12 md:w-4/12 ">
     <label htmlFor="email" className="block mb-2 text-sm font-medium text-eerie-black ">Your email</label>
-    <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-eerie-black text-sm rounded-lg focus:ring-robin-egg-blue focus:border-robin-egg-blue block w-full p-2.5 " placeholder="name@stud.ntnu.no" required/>
+    <input type="email" id="email" className="bg-white border border-gray-300 text-eerie-black text-sm rounded-lg focus:ring-robin-egg-blue focus:border-robin-egg-blue block w-full p-2.5 " placeholder="name@stud.ntnu.no" required/>
   </div>
   <div className="mb-6 w-11/12 md:w-4/12">
     <label htmlFor="password" className="block mb-2 text-sm font-medium text-eerie-black ">Your password</label>
-    <input type="password" id="password" className="bg-gray-50 border border-gray-300 text-eerie-black text-sm rounded-lg focus:ring-robin-egg-blue focus:border-robin-egg-blue block w-full p-2.5 " required/>
+    <input type="password" id="password" className="bg-white border border-gray-300 text-eerie-black text-sm rounded-lg focus:ring-robin-egg-blue focus:border-robin-egg-blue block w-full p-2.5 " required/>
   </div>
   <div className="flex items-start mb-6">
     <div className="flex items-center h-5">
-      <input id="remember" type="checkbox" value="" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:robin-egg-blue " required/>
+      <input id="remember" type="checkbox" value="" className="w-4 h-4 border border-gray-300 rounded bg-white focus:ring-3 focus:robin-egg-blue " required/>
     </div>
     <label htmlFor="remember" className="ml-2 text-sm font-medium text-gray-900 ">Remember me</label>
   </div>
--- app/src/components/UserInfo.jsx
+++ app/src/components/UserInfo.jsx
@@ -16,13 +16,13 @@ export default function UserInfo() {
       <h3 className="text-2xl font-bold text-eerie-black flex flex-col justify-center items-center mt-5">Kari Nordmann</h3>
 
       <div className="mb-6 w-96">
-        <label htmlFor="email" className="block mb-2 text-sm font-medium text-eerie-black ">Name</label>
-        <input type="email" id="disabled-input" aria-label="disabled input" className="bg-white border border-gray-300 text-eerie-black text-sm rounded-lg focus:ring-robin-egg-blue focus:border-robin-egg-blue block w-full p-2.5 cursor-not-allowed" placeholder="Kari" required />
+        <label htmlFor="name" className="block mb-2 text-sm font-medium text-eerie-black ">Name</label>
+        <input type="name" id="disabled-input" aria-label="disabled input" className="bg-white border border-gray-300 text-eerie-black text-sm rounded-lg focus:ring-robin-egg-blue focus:border-robin-egg-blue block w-full p-2.5 cursor-not-allowed" placeholder="Kari" required />
       </div>
 
       <div className="mb-6 w-96  ">
-        <label htmlFor="email" className="block mb-2 text-sm font-medium text-eerie-black ">Last name</label>
-        <input type="email" id="email" className="bg-white border border-gray-300 text-eerie-black text-sm rounded-lg focus:ring-robin-egg-blue focus:border-robin-egg-blue block w-full p-2.5 cursor-not-allowed " placeholder="Nordmann" required />
+        <label htmlFor="lname" className="block mb-2 text-sm font-medium text-eerie-black ">Last name</label>
+        <input type="name" id="lname" className="bg-white border border-gray-300 text-eerie-black text-sm rounded-lg focus:ring-robin-egg-blue focus:border-robin-egg-blue block w-full p-2.5 cursor-not-allowed " placeholder="Nordmann" required />
       </div>
 
       <div className="mb-6 w-96 ">
@@ -31,17 +31,17 @@ export default function UserInfo() {
       </div>
 
       <div className="mb-6 w-96 ">
-        <label htmlFor="email" className="block mb-2 text-sm font-medium text-eerie-black ">Password</label>
-        <input type="email" id="email" className="bg-white border border-gray-300 text-eerie-black text-sm rounded-lg focus:ring-robin-egg-blue focus:border-robin-egg-blue block w-full p-2.5 cursor-not-allowed" placeholder="*******" required />
+        <label htmlFor="password" className="block mb-2 text-sm font-medium text-eerie-black ">Password</label>
+        <input type="password" id="password" className="bg-white border border-gray-300 text-eerie-black text-sm rounded-lg focus:ring-robin-egg-blue focus:border-robin-egg-blue block w-full p-2.5 cursor-not-allowed" placeholder="*******" required />
       </div>
 
       <div className="mb-6 w-96  ">
-        <label htmlFor="email" className="block mb-2 text-sm font-medium text-eerie-black ">New password</label>
-        <input type="email" id="email" className="bg-white border border-gray-300 text-eerie-black text-sm rounded-lg focus:ring-robin-egg-blue focus:border-robin-egg-blue block w-full p-2.5 cursor-not-allowed " placeholder="*******" required />
+        <label htmlFor="newpassword" className="block mb-2 text-sm font-medium text-eerie-black ">New password</label>
+        <input type="password" id="newpassword" className="bg-white border border-gray-300 text-eerie-black text-sm rounded-lg focus:ring-robin-egg-blue focus:border-robin-egg-blue block w-full p-2.5 cursor-not-allowed " placeholder="*******" required />
       </div>
 
       <div className="flex flex-col justify-center items-center">
-        <Button text="Submit changes" />
+        <Button text="Edit" />
       </div>
     </form>
 
