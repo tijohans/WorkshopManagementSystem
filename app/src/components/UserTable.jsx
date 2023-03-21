be09a58daa2192b05246f06391c11384b2bc04db
lexavan <alexaev@stud.ntnu.no>
2023-03-21 13:05:36 +0100

    Created table for user page

---
+++ app/src/components/UserTable.jsx
@@ -0,0 +1,58 @@
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import ReactLoading from 'react-loading'
+
+
+export default function UserTable() {
+
+    return (
+       
+<div className="relative overflow-x-auto md:bottom-36 ">
+<h2 className="text-2xl font-bold text-eerie-black flex flex-col justify-center items-center md:mt-40">Current bookings</h2>
+<div className="relative overflow-x-auto shadow-md sm:rounded-lg mt-6">
+    <table className="w-full text-sm text-left text-gray-500  ">
+        <thead className="text-xs text-white uppercase bg-plum ">
+            <tr >
+                <th scope="col" class="px-6 py-3">
+                    Booked tools
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Start
+                </th>
+
+                <th scope="col" class="px-6 py-3">
+                    End
+                </th>
+              
+                <th scope="col" class="px-6 py-3">
+                    Change booking
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr className="bg-white border-b hover:bg-ghost-white  ">
+                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap  ">
+                  3D printer
+                </th>
+                <td class="px-6 py-4">
+                   20.03.23 
+                </td>
+
+                <td class="px-6 py-4">
+                  22.03.23
+                </td>
+              
+                <td class="px-6 py-4">
+                    <a href="#" className="font-medium text-plum transition ease-in-out hover:delay-50 duration-500 hover:underline hover:text-eerie-black underline-offset-4">Remove</a>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</div></div>
+
+    );
+
+
+
+}
