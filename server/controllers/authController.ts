bd30f7ebb8ee462d12761101ba6f7c7a539a0ec9
tijohans <tijohans@stud.ntnu.no>
2023-03-23 15:04:23 +0100

    comments

--- server/controllers/authController.ts
+++ server/controllers/authController.ts
@@ -37,33 +37,41 @@ const registerUser = async (req: Request, res: Response) => {
 
 /*  
     @route  POST /api/users/login
-    @desc   Route for loggin in users
+    @desc   Route for logging in users
 */
 const loginUser = async (req: Request, res: Response) => {
+
+    // Getting the email, and the password from the request body
     const {email, password} = req.body
 
+    // Getting the user from the db, based on the email
     const {data, error} = await supabase
         .from('users')
         .select('*')
         .eq('email', email)
     
+    // Returning if an error occurs
     if(error) {
         res.json({err: error})
         return
     }
 
+    // Setting the user to the first index of the data, since supabase returns an array of objects
     const user = data[0]
 
+    // Comparing the password sent from the user with the one in the database
     const correctPassword = await bcrypt.compare(password, user.password)
 
+    // Checking if either the user, or if the password is incorrect
     if(!user || !correctPassword) {
         res.status(400).json({msg: 'Username of password incorrect'})
         return
     }
 
+    // Signing a JWT with the user id, JWT_SECRET, and that it expires in 7 days
     const token = jwt.sign({sub: user.id}, String(process.env.JWT_SECRET), {expiresIn: '7d'})
     
-
+    // Finally sending the user id and the token back to the request origin
     res.status(200).send({userId: user.id, token})
 }
 
