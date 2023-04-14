3a05067f8c424ee0a943bd8608873eacb825ea10
Nicolas <94457574+nxtseq@users.noreply.github.com>
2023-04-14 13:45:05 +0200

    Fixed table footer onlick and removed 1 of x page

--- app/src/components/Table/TableFooter.jsx
+++ app/src/components/Table/TableFooter.jsx
@@ -7,13 +7,12 @@ const TableFooter = ({ range, setPage, page, slice }) => {
       }
     }, [slice, page, setPage]);
     return (
-        <nav className="flex items-center justify-between pt-4" aria-label="Table navigation">
-        <span className="text-sm font-normal text-gray-500 ">Showing <span className="font-semibold text-gray-900 ">1-10</span> of <span className="font-semibold text-gray-900 ">1000</span></span>
+        <nav className="flex items-center pt-4" aria-label="Table navigation">
         <ul className="inline-flex items-center -space-x-px">
             {range.map((el, index) => (
-            <li key={index}>
-                    <a onClick={() => setPage(el)} className="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-ghost-white hover:text-gray-700 ">{el}</a>
-                </li>
+            <li className="cursor-pointer" key={index}>
+                <a onClick={() => setPage(el)} className="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-ghost-white hover:text-gray-700 cursor-pointer">{el}</a>
+            </li>
             ))}
         </ul>
       </nav>
