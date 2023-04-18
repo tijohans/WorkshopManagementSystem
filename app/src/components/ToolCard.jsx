9c566527d7637c85c59f4d83c64169350b7c970b
Haavaala <haavaala@stud.ntnu.no>
2023-04-18 10:28:52 +0200

    did some styling adjustments on the toolcard

--- app/src/components/ToolCard.jsx
+++ app/src/components/ToolCard.jsx
@@ -4,7 +4,7 @@ import EditButton from "./EditButton";
 export default function ToolCard(props) {
   return (
     <div className=" bg-white flex flex-col rounded-2xl justify-evenly border-2 h-auto md:flex-row md:w-96 md:h-80">
-        <div className="w-52 h-72 md:w-auto md:h-52">
+        <div className="w-52 min-h-54 md:w-auto md:h-52">
         <img
           className="w-52 h-40 mt-1 m-1.5 object-cover rounded-2xl aspect-square justify-center"
           src={props.src}
@@ -14,12 +14,12 @@ export default function ToolCard(props) {
             {props.name}
           </h1>
         </div>
-        <div className="md:max-w-max">
-          <h1 className=" hidden text-3xl font-bold ml-3 w-52 md:w-28 md:text-2xl md:block">
+        <div className="md:max-w-max md:flex-row md:justify-between">
+          <h1 className=" hidden text-3xl font-bold ml-3 w-52 md:w-28 md:text-2xl md:block md:mb-4">
             {props.name}
           </h1>
-          <p className="ml-3 w-52 md:w-36 md:text-sm">{props.paragraph}</p>
-          <div className="w-10">
+          <p className="ml-3 w-52 md:w-36 md:text-sm md:mb-3">{props.paragraph}</p>
+          <div className="w-10 md: flex flex-end flex-col">
             <SmallButton text="Book" toolId={props.id} />
             <EditButton text="Edit" toolId={props.id}></EditButton>
           </div>
