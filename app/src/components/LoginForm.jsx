ca8818a07a57290691e9d5d25f7a6d0be3258297
tijohans <tijohans@stud.ntnu.no>
2023-04-19 13:32:25 +0200

    Made login on front-end work, had to do some adjustments in backend

--- app/src/components/LoginForm.jsx
+++ app/src/components/LoginForm.jsx
@@ -1,54 +1,118 @@
-import React, { ReactNode, useState } from 'react'
-import Button from './Button'
-import SmallButton from './SmallButton'
+import React, { useState } from 'react'
 import { useForm } from 'react-hook-form';
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
+import Cookie from 'universal-cookie'
+import DangerWarning from '../components/DangerWarning.jsx'
 
 
 export default function LoginForm() {
 
-  
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
-  
-  
-  const onSubmit = (data) => {
-    const userData = JSON.parse(localStorage.getItem(data.email));
-    if (userData) { // getItem can return actual value or null
-      if (userData.password === data.password) {
-        console.log(userData.name + " You Are Successfully Logged In");
-      } else {
-        console.log("Email or Password doesn't match");
-      }
-    } else {
-      console.log("Email or Password doesn't match.");
+    const [login, setLogin] = useState(true)
+
+    const navigate = useNavigate()
+    const {
+        register,
+        handleSubmit,
+        setError
+    } = useForm();
+
+    const onSubmit = async (userData) => {
+        setLogin(true)
+        console.log('onsubmit')
+        console.log(userData)
+        try {
+            console.log('try')
+            const response = await axios.post('http://localhost:9003/api/login', {
+              email: userData.email,
+              password: userData.password
+            })
+
+            const token = response.data.token.split(' ')[1]
+
+            const cookie = new Cookie()
+            cookie.set('token', token)
+
+            navigate('/userpage')
+          } catch (error) {
+            setLogin(false)
+            if (error.response) {
+              const { status, data } = error.response;
+              if (status === 400 && data && data.err) {
+                setError("email", { type: "manual", message: data.err });
+              } else if (status === 401) {
+                setError("email", { type: "manual", message: "Invalid email or password" });
+              }
+            }
+            console.error(error);
+          }
     }
-  };
 
     return (
-       
-      <form className="top-1 h-200 flex flex-col justify-center items-center space-y-10" onSubmit={handleSubmit((data) => console.log(data))}>
-        <h2 className="text-3xl font-bold text-eerie-black md:text-5xl ">User Login</h2>
-          <div className="mb-6 w-11/12 md:w-4/12 ">
-            <label htmlFor="email" className="block mb-2 text-sm font-medium text-eerie-black ">Your email</label>
-            <input type="email" id="email" className="bg-white border border-gray-300 text-eerie-black text-sm rounded-lg focus:ring-robin-egg-blue focus:border-robin-egg-blue block w-full p-2.5 " placeholder="name@stud.ntnu.no" required {...register("email", { required: true })}/>
-          </div>
-          <div className="mb-6 w-11/12 md:w-4/12">
-            <label htmlFor="password" className="block mb-2 text-sm font-medium text-eerie-black ">Your password</label>
-            <input type="password" id="password" className="bg-white border border-gray-300 text-eerie-black text-sm rounded-lg focus:ring-robin-egg-blue focus:border-robin-egg-blue block w-full p-2.5 " required {...register("password")}/>
-          </div>
-          <div className="flex items-start mb-6">
-            <div className="flex items-center h-5">
-              <input id="remember" type="checkbox" value="" className="w-4 h-4 border border-gray-300 rounded bg-white focus:ring-3 focus:robin-egg-blue " required/>
+
+        <form 
+            onSubmit={handleSubmit(onSubmit)}
+            className="top-1 h-200 flex flex-col justify-center items-center space-y-10" 
+            >
+            <h2 className="text-3xl font-bold text-eerie-black md:text-5xl ">User Login</h2>
+            <div className="mb-6 w-11/12 md:w-4/12 ">
+                <label htmlFor="email" className="block mb-2 text-sm font-medium text-eerie-black ">Your email</label>
+                <input
+                    type="email"
+                    id="email"
+                    className="bg-white border border-gray-300 text-eerie-black text-sm rounded-lg focus:ring-robin-egg-blue focus:border-robin-egg-blue block w-full p-2.5 "
+                    placeholder="name@stud.ntnu.no"
+                    required 
+                    {...register("email", { required: true })} />
             </div>
-            <label htmlFor="remember" className="ml-2 text-sm font-medium text-gray-900 ">Remember me</label>
-          </div>
-         <Button text="Login" link="../userpage"/>
-         <p className="">Need help? Contact us blablabla.</p>
-      </form>
-   
-    
+            <div className="mb-6 w-11/12 md:w-4/12">
+                <label
+                    htmlFor="password"
+                    className="block mb-2 text-sm font-medium text-eerie-black ">
+                    Your password
+                </label>
+                <input
+                    type="password"
+                    id="password"
+                    className="bg-white border border-gray-300 text-eerie-black text-sm rounded-lg focus:ring-robin-egg-blue focus:border-robin-egg-blue block w-full p-2.5 "
+                    required
+                    {...register("password", { required: true })} />
+            </div>
+            {!login ? <DangerWarning text="Wrong username or password" /> : ''}
+            <div className="flex items-start mb-6">
+                <div className="flex items-center h-5">
+                    <input id="remember" type="checkbox" value="" className="w-4 h-4 border border-gray-300 rounded bg-white focus:ring-3 focus:robin-egg-blue" />
+                </div>
+                <label htmlFor="remember" className="ml-2 text-sm font-medium text-gray-900 ">Remember me</label>
+            </div>
+            <p className="">Need help? Contact us blablabla.</p>
+            <input type="submit" value="login" />
+        </form>
     )
 }
+
+
+
+/* 
+    HOok form examplke
+*/
+
+// export function example() {
+//     const { register, handleSubmit } = useForm();
+//     const [data, setData] = useState("");
+
+//     return (
+//         <form onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}>
+//             <Header />
+//             <input {...register("firstName")} placeholder="First name" />
+//             <select {...register("category", { required: true })}>
+//                 <option value="">Select...</option>
+//                 <option value="A">Option A</option>
+//                 <option value="B">Option B</option>
+//             </select>
+//             <textarea {...register("aboutYou")} placeholder="About you" />
+//             <p>{data}</p>
+//             <input type="submit" />
+//         </form>
+//     );
+// }
