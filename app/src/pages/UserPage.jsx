8e8149bad1969f39557c8da421d470589e22df6f
lexavan <alexaev@stud.ntnu.no>
2023-04-19 11:07:24 +0200

    Made changes to the user page

--- app/src/pages/UserPage.jsx
+++ app/src/pages/UserPage.jsx
@@ -2,23 +2,35 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import Button from '../components/Button'
 import UserInfo from '../components/UserInfo'
-
+import CheckList from '../components/CheckList'
 import UserTable from '../components/UserTable'
+import CourseTable from '../components/CourseTable'
 
 export default function UserPage() {
 
     return (
         <>
-           <h1 className="flex  md:flex-column justify-center items-center text-4xl md:text-5xl font-bold text-eerie-black mb-20">Account information</h1>
-        <div className="md:flex  md:flex-column justify-center items-center md:space-x-16">
+           <h1 className="flex  md:flex-column justify-center  text-4xl md:text-5xl font-bold text-eerie-black mb-20">Account information</h1>
+        <div className="md:flex  md:flex-column justify-center  md:space-x-16">
            
             <UserInfo />
-
-          
-            <UserTable /> {/*Denne UserInfo taggen skal erstattes med Table */}
+     
+   <div className="flex justify-center top-15">
+         <CourseTable />
         </div>
 
-        <p className="italic flex justify-center" >If your account information is incorrect, please contact the page manager. </p>
+
+
+    
+         
+        </div>
+<div className="md:flex  md:justify-center">
+<UserTable />
+      
+    </div>
+    
+
+        <p className="italic flex justify-center mt-20" >If your account information is incorrect, please contact the page manager. </p>
 
        
 </>
