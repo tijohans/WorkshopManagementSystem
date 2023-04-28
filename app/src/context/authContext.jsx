c2625e4fb01250585f6d282175d213a0f2f130fa
tijohans <tijohans@stud.ntnu.no>
2023-04-28 13:48:21 +0200

    Made login route send userrole in jwt, and added userrole to the authcontext

--- app/src/context/authContext.jsx
+++ app/src/context/authContext.jsx
@@ -1,14 +1,25 @@
 import { createContext, useEffect, useState } from "react"
 import Cookie from "universal-cookie"
+import jwt_decode from 'jwt-decode'
 
 const AuthContext = createContext()
 
 const AuthProvider = ({children}) => {
     const cookie = new Cookie()
     const token = cookie.get('token')
+    const [userRole, setUserRole] = useState(NaN)
+
+    useEffect(() => {
+        if(!token)
+            return 
+            
+        const decodedToken = jwt_decode(token)
+        
+        setUserRole(decodedToken.role)
+    }, [token])
 
     return (
-        <AuthContext.Provider value={{token}}>
+        <AuthContext.Provider value={{token, userRole}}>
             {children}
         </AuthContext.Provider>
     )
