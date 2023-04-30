2b5b2c49e613ad2ab909dd4d13500a8204550159
Nicolas <94457574+nxtseq@users.noreply.github.com>
2023-04-30 18:49:06 +0200

    wayy

--- app/src/pages/AdminDashboard.jsx
+++ app/src/pages/AdminDashboard.jsx
@@ -4,36 +4,55 @@ import Button from "../components/Button"
 import React, { useState } from 'react'
 import AdminReports from "../components/AdminReports"
 
-export default function AdminDashboard() {
-    const [toggleFunc, setToggleFunc] = useState()
-    return (
-        <>
-            <header className="flex flex-col justify-center align-middle items-center">
-                <h1 className="text-3xl font-bold text-eerie-black md:text-4xl ">Admin Dashboard</h1>
-                
-            </header>
-
-            <section className="flex justify-center align-middle items-center flex-col m-2">
-            <hr class="w-96 h-px my-3 bg-gray-300 border-0" />
-                <Button size="small" clickFunction={() => { setToggleFunc(!toggleFunc) }}>{toggleFunc ? "See users" : "See reports"}</Button>
-                <hr class="w-96 h-px my-3 bg-gray-300 border-0" />
-            </section>
-
-            {toggleFunc ?
-                <section className="flex flex-col justify-center text-center align-middle m-2">
-                    <h1 className=" text-2xl md:text-3xl">Reports:</h1>
-                    <AdminReports />
-                </section>
-
-                :
+const RenderDashboard = ({select}) => {
+    switch (select) {
+        case "users":
+            return (
                 <section className="flex justify-center align-middle items-center flex-col m-2">
                     <h1 className=" text-2xl md:text-3xl">Registered users:</h1>
                     <Table name="users" rowsPerPage={15} footerButton={true} footerButtonLink={"/admin/user"} footerButtonText={"Create new user"} />
                 </section>
-            }
+            )
 
+        case "reports":
+            return (
+                <section className="flex flex-col justify-center text-center align-middle m-2">
+                    <h1 className=" text-2xl md:text-3xl">Reports:</h1>
+                    <AdminReports />
+                </section>
+            )
 
+        case "bookings":
+            return (
+                <section className="flex justify-center align-middle items-center flex-col m-2">
+                    <h1 className=" text-2xl md:text-3xl">Current bookings:</h1>
+                    <Table name="bookings" rowsPerPage={15} />
+                </section>
+            )
+    }
+}
 
+export default function AdminDashboard() {
+    const [select, setSelect] = useState("reports")
+    const [rows, setRows] = useState(15)
+
+    
+
+    return (
+        <>
+            <header className="flex flex-col justify-center align-middle items-center">
+                <h1 className="text-3xl font-bold text-eerie-black md:text-4xl ">Admin Dashboard</h1>
+            </header>
+
+            <section className="flex justify-center align-middle items-center flex-col md:flex-row m-5">
+                <hr class="w-96 h-px my-3 bg-gray-300 border-0" />
+                <Button size="small" clickFunction={() => { setSelect("reports")}}>See reports</Button>
+                <Button size="small" clickFunction={() => { setSelect("users")}}>See users</Button>
+                <Button size="small" clickFunction={() => { setSelect("bookings")}}>See bookings</Button>
+                <hr class="w-96 h-px my-3 bg-gray-300 border-0" />
+            </section>
+
+            <RenderDashboard select={select}/>
 
         </>
     )
