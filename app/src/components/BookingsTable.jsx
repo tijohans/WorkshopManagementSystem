f882113486920c96b1cad58bd89b0e4d8011b6b7
tijohans <tijohans@stud.ntnu.no>
2023-04-30 18:14:46 +0200

    fixed backend bug where data was sent in data oject

--- app/src/components/BookingsTable.jsx
+++ app/src/components/BookingsTable.jsx
@@ -5,102 +5,104 @@ import axios from 'axios'
 
 export default function BookingsTable() {
 
-const [booking, setBooking] = useState([])
-const [data] = useState([])
+    const [booking, setBooking] = useState([])
+    const [data] = useState([])
 
 
 
-useEffect(() => {
-  getBooking()
-}, []);
+    useEffect(() => {
+        getBooking()
+    }, []);
 
 
 
-  const getBooking = () => {
-    axios.get(`https://wms-api-ps1s.onrender.com/api/bookings/bookingwithtoolname`) 
-      .then((response) => {
-        setBooking(response.data)
-       
-      })
-      .catch(error => console.error("Error: " + error))
-  }
+    const getBooking = () => {
+        axios.get(`http://localhost:9003/api/bookings/bookingwithtoolname`)
+            .then((response) => {
 
-console.log(booking)
+                console.log(response.data)
+                setBooking(response.data)
 
+            })
+            .catch(error => console.error("Error: " + error))
+    }
 
-  
-  return (
+    console.log(booking)
 
 
 
-   
-<div className="relative overflow-x-auto  ">
-
-
-{data.map((item) => (
-
-
-<p>{item.booking_start}</p>
-))}
-
-<h2 className="text-2xl font-bold text-eerie-black flex flex-col justify-center items-center">My bookings</h2>
-<div className="relative overflow-x-auto shadow-md sm:rounded-lg mt-6">
-    <table className="w-full text-sm text-left text-gray-500  ">
-        <thead className="text-xs text-white uppercase bg-plum ">
-            <tr >
-                <th scope="col" className="px-6 py-3">
-                    Booked tool ID
-                </th>
-                <th scope="col" className="px-6 py-3">
-                    User ID
-                </th>
-
-                <th scope="col" className="px-6 py-3">
-                    Date
-                </th>
-
-                <th scope="col" className="px-6 py-3">
-                    Start time
-                </th>
-              
-                <th scope="col" className="px-6 py-3">
-                    End time
-                </th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr className="bg-white border-b hover:bg-ghost-white  ">
-                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap  ">
-                
-                </th>
-                <td className="px-6 py-4">
-                
-                </td>
-
-                <td className="px-6 py-4">
-               
-                </td>
-
-
-                <td className="px-6 py-4">
-                 
-                </td>
-              
-                <td className="px-6 py-4">
-                
-                </td>
-              
-            
-            </tr>
-        </tbody>
-    </table>
-</div>
+    return (
 
 
 
-</div>
+
+        <div className="relative overflow-x-auto  ">
+
+
+            {/* {booking.map((item) => (
+
+
+                <p>{item.booking_start}</p>
+            ))} */}
+
+            <h2 className="text-2xl font-bold text-eerie-black flex flex-col justify-center items-center">My bookings</h2>
+            <div className="relative overflow-x-auto shadow-md sm:rounded-lg mt-6">
+                <table className="w-full text-sm text-left text-gray-500  ">
+                    <thead className="text-xs text-white uppercase bg-plum ">
+                        <tr >
+                            <th scope="col" className="px-6 py-3">
+                                Booked tool ID
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                User ID
+                            </th>
+
+                            <th scope="col" className="px-6 py-3">
+                                Date
+                            </th>
+
+                            <th scope="col" className="px-6 py-3">
+                                Start time
+                            </th>
+
+                            <th scope="col" className="px-6 py-3">
+                                End time
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr className="bg-white border-b hover:bg-ghost-white  ">
+                            <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap  ">
+
+                            </th>
+                            <td className="px-6 py-4">
+
+                            </td>
+
+                            <td className="px-6 py-4">
+
+                            </td>
+
+
+                            <td className="px-6 py-4">
+
+                            </td>
+
+                            <td className="px-6 py-4">
+
+                            </td>
+
+
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
 
 
 
-  )
+        </div>
+
+
+
+    )
 }
