a472e97d7d4423eb4b7beeb6e0bcf4b0d4f85814
Haavaala <haavaala@stud.ntnu.no>
2023-05-01 13:38:33 +0200

    formatted the other documents too

--- app/src/components/UserInfo.jsx
+++ app/src/components/UserInfo.jsx
@@ -1,60 +1,63 @@
-import React, { ReactNode, useState, useContext, useEffect } from 'react'
-import { AuthContext } from '../context/authContext'
-import jwt_decode from 'jwt-decode'
-import axios from 'axios'
-import ReactLoading from 'react-loading'
+import React, { ReactNode, useState, useContext, useEffect } from "react";
+import { AuthContext } from "../context/authContext";
+import jwt_decode from "jwt-decode";
+import axios from "axios";
+import ReactLoading from "react-loading";
 
 export default function UserInfo() {
-    const { token } = useContext(AuthContext)
-    const userId = jwt_decode(token).sub
-    const [user, setUser] = useState([])
-    const [loading, setLoading] = useState(true)
+  const { token } = useContext(AuthContext);
+  const userId = jwt_decode(token).sub;
+  const [user, setUser] = useState([]);
+  const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    getUser();
+  }, []);
 
+  const getUser = () => {
+    axios
+      .get(`https://wms-api-ps1s.onrender.com/api/users/${userId}`, {
+        headers: { "x-access-token": token },
+      })
+      .then((response) => {
+        setUser(response.data[0]);
+        setLoading(false);
+      })
+      .catch((error) => console.error("Error: " + error));
+  };
 
-    useEffect(() => {
-        getUser()
-    }, []);
-
-
-    const getUser = () => {
-        axios.get(`https://wms-api-ps1s.onrender.com/api/users/${userId}`, { headers: { "x-access-token": token } })
-            .then((response) => {
-                setUser(response.data[0])
-                setLoading(false)
-            })
-            .catch(error => console.error("Error: " + error))
-    }
-
-
-    return (
-        <>
-        {loading ? (
-            <div className="flex justify-center mt-20">
-            <ReactLoading type="spin" color="#9C528B" />
-            </div>
-            ) : (
-
-<div className="bg-white px-9 shadow-md rounded-lg h-max pb-8">
-            <h3 className="text-2xl font-bold text-eerie-black flex flex-col justify-center items-center mt-5"> </h3>
+  return (
+    <>
+      {loading ? (
+        <div className="flex justify-center mt-20">
+          <ReactLoading type="spin" color="#9C528B" />
+        </div>
+      ) : (
+        <div className="bg-white px-9 shadow-md rounded-lg h-max pb-8">
+          <h3 className="text-2xl font-bold text-eerie-black flex flex-col justify-center items-center mt-5">
+            {" "}
+          </h3>
           {/*   <div className="flex flex-col justify-center">
                 <div className="flex flex-col justify-center mb-10 md:mb-42 h-fit">
 */}
-                    <h3 className="text-2xl py-5 font-bold text-eerie-black flex flex-col justify-center items-center mt-5">{user.first_name} {user.last_name}</h3>
+          <h3 className="text-2xl py-5 font-bold text-eerie-black flex flex-col justify-center items-center mt-5">
+            {user.first_name} {user.last_name}
+          </h3>
 
-
-<ol>
-    <li className="py-5"><span className="font-bold">First name: </span> {user.first_name}</li>
-    <li className="py-5"><span className="font-bold">Last name: </span>{user.last_name}</li>
-    <li className="py-5"><span className="font-bold">Email:</span> {user.email}</li>
-    </ol>
-    </div>
-
-                  
-
-            )}
-
-     
-        </>
-    )
+          <ol>
+            <li className="py-5">
+              <span className="font-bold">First name: </span> {user.first_name}
+            </li>
+            <li className="py-5">
+              <span className="font-bold">Last name: </span>
+              {user.last_name}
+            </li>
+            <li className="py-5">
+              <span className="font-bold">Email:</span> {user.email}
+            </li>
+          </ol>
+        </div>
+      )}
+    </>
+  );
 }
--- app/src/components/UserTable.jsx
+++ app/src/components/UserTable.jsx
@@ -22,7 +22,7 @@ export default function UserTable() {
       })
       .then((response) => {
         setUserBooking(response.data);
-        setLoading(false)
+        setLoading(false);
       })
       .catch((error) => console.error("Error: " + error));
   };
@@ -48,71 +48,69 @@ export default function UserTable() {
 
   return (
     <>
-        {loading ? (
-            <div className="flex justify-center mt-20">
-            <ReactLoading type="spin" color="#9C528B" />
-            </div>
-            ) : (
+      {loading ? (
+        <div className="flex justify-center mt-20">
+          <ReactLoading type="spin" color="#9C528B" />
+        </div>
+      ) : (
+        <div className="overflow-x-auto">
+          <h2 className="text-2xl font-bold text-eerie-black flex flex-col justify-center items-center">
+            My bookings
+          </h2>
+          <div className="overflow-x-auto shadow-md sm:rounded-lg mt-6">
+            <table className="w-full text-sm text-left text-gray-500  ">
+              <thead className="text-xs text-white uppercase bg-plum ">
+                <tr>
+                  <th scope="col" className="px-6 py-3">
+                    Booked tool ID
+                  </th>
+                  <th scope="col" className="px-6 py-3">
+                    Date
+                  </th>
 
-    
-    <div className="overflow-x-auto">
-      <h2 className="text-2xl font-bold text-eerie-black flex flex-col justify-center items-center">
-        My bookings
-      </h2>
-      <div className="overflow-x-auto shadow-md sm:rounded-lg mt-6">
-        <table className="w-full text-sm text-left text-gray-500  ">
-          <thead className="text-xs text-white uppercase bg-plum ">
-            <tr>
-              <th scope="col" className="px-6 py-3">
-                Booked tool ID
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Date
-              </th>
+                  <th scope="col" className="px-6 py-3">
+                    Start time
+                  </th>
 
-              <th scope="col" className="px-6 py-3">
-                Start time
-              </th>
+                  <th scope="col" className="px-6 py-3">
+                    End time
+                  </th>
 
-              <th scope="col" className="px-6 py-3">
-                End time
-              </th>
+                  <th scope="col" className="px-6 py-3">
+                    Change booking
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {userBooking.map((item) => (
+                  <tr className="bg-white border-b hover:bg-ghost-white  ">
+                    <th
+                      scope="row"
+                      className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap  "
+                    >
+                      <td className="px-6 py-4">{item.booking_id}</td>
+                    </th>
+                    <td className="px-6 py-4">{item.booking_date}</td>
 
-              <th scope="col" className="px-6 py-3">
-                Change booking
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {userBooking.map((item) => (
-              <tr className="bg-white border-b hover:bg-ghost-white  ">
-                <th
-                  scope="row"
-                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap  "
-                >
-                  <td className="px-6 py-4">{item.booking_id}</td>
-                </th>
-                <td className="px-6 py-4">{item.booking_date}</td>
+                    <td className="px-6 py-4">{item.booking_start}</td>
 
-                <td className="px-6 py-4">{item.booking_start}</td>
+                    <td className="px-6 py-4">{item.booking_end}</td>
 
-                <td className="px-6 py-4">{item.booking_end}</td>
-
-                <td className="px-6 py-4">
-                  <a
-                    onClick={() => deleteBooking(item.booking_id)}
-                    className="font-medium text-plum transition ease-in-out hover:delay-50 duration-500 hover:underline hover:text-eerie-black underline-offset-4"
-                  >
-                    Return tool
-                  </a>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    </div>
-    )}
+                    <td className="px-6 py-4">
+                      <a
+                        onClick={() => deleteBooking(item.booking_id)}
+                        className="font-medium text-plum transition ease-in-out hover:delay-50 duration-500 hover:underline hover:text-eerie-black underline-offset-4"
+                      >
+                        Return tool
+                      </a>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      )}
     </>
   );
 }
